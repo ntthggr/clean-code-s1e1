fd86fa520ad1df1e3d18b551bbff6da3d7a2d43f
ntthggr <ntthggr@gmail.com>
2022-10-31 16:13:28 +0500

    fix: add id according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput = document.getElementById('new-task'); //Add a new task.
-var addButton = document.getElementsByTagName('button')[0]; //first button
+var addButton = document.getElementById('add-button'); //first button
 var incompleteTaskHolder = document.getElementById('todo-tasks'); //ul of #incompleteTasks
 var completedTasksHolder = document.getElementById('completed-tasks'); //completed-tasks
 
